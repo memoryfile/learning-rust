b5a174cd100b062ecc36a9ff98323037c330b57e
memoryfile <cooperlewis@protonmail.com>
2022-07-22 18:53:24 -0400

    Create fibonacci function and input

--- fibonacci/src/main.rs
+++ fibonacci/src/main.rs
@@ -1,3 +1,26 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Input a number.");
+    let mut number = String::new();
+
+    io::stdin()
+        .read_line(&mut number)
+        .expect("Failed to read line");
+
+    let number2 = match number.parse::<u32>() {
+        Ok(number2) => number2,
+        Err(_e) => 1,
+    };
+
+    fib(number2);
+}
+
+fn fib(num: u32) -> u32 {
+    match num {
+        0 => 1,
+        1 => 1,
+        _ => fib(num - 1) + fib(num - 2),
+    }
+    // println!("{}", fib(num));
 }
