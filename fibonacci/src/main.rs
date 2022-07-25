86c480d114b11ea8559ca72783acb2b6857c2288
memoryfile <cooperlewis@protonmail.com>
2022-07-25 17:06:42 -0400

    Rewrite

--- fibonacci/src/main.rs
+++ fibonacci/src/main.rs
@@ -1,26 +1,49 @@
+// Code following the tutorials of...
+// https://benjaminbrandt.com/fibonacci-in-rust/
+// https://dev.to/kelvinkirima014/generating-the-nth-fibonacci-in-rust-238k
+
 use std::io;
 
-fn main() {
-    println!("Input a number.");
-    let mut number = String::new();
-
-    io::stdin()
-        .read_line(&mut number)
-        .expect("Failed to read line");
-
-    let number2 = match number.parse::<u32>() {
-        Ok(number2) => number2,
-        Err(_e) => 1,
-    };
-
-    fib(number2);
-}
-
-fn fib(num: u32) -> u32 {
-    match num {
+fn fib(n: u32) -> u32 {
+    match n {
         0 => 1,
         1 => 1,
-        _ => fib(num - 1) + fib(num - 2),
+        2 => 1,
+        _ => (fib(n - 1)) + (fib(n - 2)),
     }
-    // println!("{}", fib(num));
+}
+
+fn to15() {
+    for int in 0..15 {
+        println!("fib ({}) => {}", int, fib(int)); // Does fib sequence 1 -> 15
+    }
+}
+
+fn main() {
+    println!("Fibonnaci generator");
+    println!("Type \"quit\" to exit, or \"to15\" to receive a 1 -> 15 fibonacci sequence!");
+
+    loop {
+        let mut n = String::new();
+
+        println!("\nEnter your input:");
+
+        io::stdin().read_line(&mut n).expect("Failed to read line");
+
+        if n.trim() == "quit" {
+            break;
+        }
+
+        if n.trim() == "to15" {
+            to15();
+            break;
+        }
+
+        let n: u32 = match n.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("{}", fib(n));
+    }
 }
